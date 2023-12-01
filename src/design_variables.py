15763d892ae50479d19523eb37eb65e6b45e5ae5
simon <simon.cadavid@tum.de>
2023-12-01 09:50:42 -0500

    improved documentation and moved file

--- src/design_variables.py
+++ src/design_variables.py
@@ -72,6 +72,6 @@ def get_angles():
 
 b = 16.  # mm - Roving bandwidth
 t_R = 0.65  # mm - Laminate thickness ar r = R (cylindrical part)
-t_P = 0.35  # mm - Ply thickness
-max_y_hoop = 378.
-t_hoop = 0.72
+t_P = 0.35  # mm - Thickness of a single ply
+max_y_hoop = 378.  # Maximum y coordinate for the hoop layers
+t_hoop = 0.72  # Thickness of hoop layers
